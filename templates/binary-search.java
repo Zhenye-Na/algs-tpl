0f29abea4ec1dbc59a574c8b9aaef24a92484a00
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-07-07 01:16:31 -0500

    Binary Search

---
+++ templates/binary-search.java
@@ -0,0 +1,42 @@
+/** 
+ *  Binary Search template
+ *  @param nums an ineteger arrray sorted in ascending order.
+ *  @param target an integer
+ *  @return position of the integer in the array
+ */
+
+
+public class Solution {
+
+    public int findPosition(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+
+            if (target == nums[start]) {
+                return start;
+            }
+
+            if (target == nums[end]) {
+                return end;
+            }
+
+            return -1;
+
+        }
+
+    }
+}
