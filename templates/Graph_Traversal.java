e1ff0fa8462af2b8253cb5d4b7c8b4de68d6790f
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-07-13 16:47:07 -0500

    BFS + Graph Traversal

---
+++ templates/Graph_Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     ArrayList<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) {
+ *         label = x;
+ *         neighbors = new ArrayList<UndirectedGraphNode>();
+ *     }
+ * };
+ */
+
+
+public List<UndirectedGraphNode> getGraphNodes(UndirectedGraphNode node) {
+
+    // BFS to get all nodes
+    List<UndirectedGraphNode> graph = new ArrayList<>();
+    Set<UndirectedGraphNode> history = new HashSet<>();
+    Queue<UndirectedGraphNode> queue = new LinkedList<>();
+    queue.offer(node);
+    history.add(node);
+
+    while (!queue.isEmpty()) {
+
+        UndirectedGraphNode tmpNode = queue.poll();
+        graph.add(tmpNode);
+
+        for (UndirectedGraphNode neighbor : tmpNode.neighbors) {
+            if (!history.contains(neighbor)) {
+                queue.offer(neighbor);
+                history.add(neighbor);
+            }
+        }
+
+    }
+    return graph;
+}
