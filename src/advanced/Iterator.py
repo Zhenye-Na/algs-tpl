3c2f5aa35001728bc4307cd4c13cdd6083845403
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2019-10-16 22:21:11 -0700

    advanced data structures template: union find, trie, hashheap and iterator related

---
+++ src/advanced/Iterator.py
@@ -0,0 +1,17 @@
+class NestedIterator:
+
+    def __init__(self, args):
+        # initialize stack
+        # initialize arguments
+        pass
+
+    def next(self):
+        # return value of next_element
+        # re-assign None to next_element
+        pass
+
+    def hasNext(self):
+        # 如果 next_element 非空, 返回 True
+        # 否则找到 next_element 并记录, 返回 True
+        # 如果 stack 空了, 返回 False
+        pass
